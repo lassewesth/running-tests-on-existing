65df925c41591dbb71610858632b567fc87023c2
Lasse Westh-Nielsen <lassewesth@gmail.com>
2018-06-19 16:12:26 +0200

    seems to be good enough eh?

--- cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/DisastrousIT.java
+++ cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/DisastrousIT.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class DisastrousIT {
     @Test
     public void shouldYikesSlowly() throws Exception {
-        // seems to be good enough
+        // seems to be good enough eh?
         assertThat(new Accident().toString(), startsWith("an"));
     }
 }
