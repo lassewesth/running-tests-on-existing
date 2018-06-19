a756e037f8e0d9b7c43132de9c154852a0a131da
Lasse Westh-Nielsen <lassewesth@gmail.com>
2018-06-19 16:02:06 +0200

    sick and tired yo what??

--- cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/DisastrousIT.java
+++ cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/DisastrousIT.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class DisastrousIT {
     @Test
     public void shouldYikesSlowly() throws Exception {
-        // poke it yo what??
+        // seems to be good enough
         assertThat(new Accident().toString(), startsWith("an"));
     }
 }
