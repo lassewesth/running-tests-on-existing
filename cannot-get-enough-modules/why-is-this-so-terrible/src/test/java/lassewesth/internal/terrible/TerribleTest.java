98ee8135f2157de2fd975c82f80d143b5543fa5f
Lasse Westh-Nielsen <lassewesth@gmail.com>
2018-06-19 12:34:04 +0200

    java 10 comes along for v5.0

--- cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
+++ cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
@@ -2,13 +2,17 @@ package lassewesth.internal.terrible;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeThat;
 
 public class TerribleTest {
     @Test
     public void shouldYikes() throws Exception {
-        org.junit.Assume.assumeFalse(false || true);
+        String javaVersion = System.getProperty("java.version");
 
-        assertNotNull(new Accident());
+        assumeThat("java version is " + javaVersion, javaVersion, startsWith("1.10"));
+
+        fail("fails on java 10");
     }
 }
