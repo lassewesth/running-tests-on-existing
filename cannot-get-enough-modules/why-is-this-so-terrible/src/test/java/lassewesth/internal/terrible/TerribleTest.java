5f86bdc5e6f55f569c00d35529897c2f12f41f6f
Lasse Westh-Nielsen <lassewesth@gmail.com>
2018-06-19 13:14:13 +0200

    a commit to trigger chain

--- cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
+++ cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
@@ -2,12 +2,15 @@ package lassewesth.internal.terrible;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 
 public class TerribleTest {
     @Test
     public void shouldYikes() throws Exception {
-        org.junit.Assume.assumeFalse(false || true);
+        String javaVersion = System.getProperty("java.version");
+
+        org.junit.Assume.assumeThat("java version is " + javaVersion, javaVersion, is("1.8"));
 
         assertNotNull(new Accident());
     }
