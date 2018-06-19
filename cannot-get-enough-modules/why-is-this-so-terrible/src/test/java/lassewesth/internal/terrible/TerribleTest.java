3ad2cfe88429e26a4b6f37e577ef8bcd196dbdcd
Lasse Westh-Nielsen <lassewesth@gmail.com>
2018-06-19 13:54:25 +0200

    poke 4.7

--- cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
+++ cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
@@ -2,7 +2,6 @@ package lassewesth.internal.terrible;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertNull;
 
