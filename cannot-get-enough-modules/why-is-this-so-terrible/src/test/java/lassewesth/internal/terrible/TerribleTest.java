79018fbe40379e38d6783a733a9f2f08dfde9b49
Lasse Westh-Nielsen <lassewesth@gmail.com>
2018-06-20 11:41:12 +0200

    fix 5.1

--- cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
+++ cannot-get-enough-modules/why-is-this-so-terrible/src/test/java/lassewesth/internal/terrible/TerribleTest.java
@@ -2,6 +2,7 @@ package lassewesth.internal.terrible;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assume.assumeThat;
@@ -11,10 +12,8 @@ public class TerribleTest {
     public void shouldYikes() throws Exception {
         String javaVersion = System.getProperty("java.version");
 
-        assertThat(javaVersion, startsWith("1.10"));
+        assumeThat("java version is " + javaVersion, javaVersion, startsWith("10.0"));
 
-        assumeThat("java version is " + javaVersion, javaVersion, startsWith("1.10"));
-
-        assertThat(javaVersion, startsWith("1.11"));
+        assertThat(javaVersion, is("10.0.1"));
     }
 }
